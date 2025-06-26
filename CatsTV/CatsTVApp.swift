11c4e4fd9f7df4133860b8f186210bccd4150325
Codymullis <69050280+Codymullis@users.noreply.github.com>
2025-06-26 15:54:19 -0400

    Initial Commit

---
+++ CatsTV/CatsTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsTVApp.swift
+//  CatsTV
+//
+//  Created by Cody Mullis on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct CatsTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatsTVTests/CatsTVTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatsTVTests.swift
+//  CatsTVTests
+//
+//  Created by Cody Mullis on 6/26/25.
+//
+
+import Testing
+@testable import CatsTV
+
+struct CatsTVTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
